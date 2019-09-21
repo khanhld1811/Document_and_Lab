0c5e2ea860313fc5fe5f6bca59dc5759a35d9022
DUY KHANH <khanhlerduy@gmail.com>
2019-09-21 18:05:44 +0700

    Add file variable, data_type, object

---
+++ Learn_javascript/Learn_width_video/Javascript_basic/3_Data_types/datatype.js
@@ -0,0 +1,25 @@
+// Number
+var a1 = 100;
+var a2 = -0.5;
+
+// String
+var str  = 'Hello, world.';
+var str2 = "Hello, world.";
+console.log(str2);
+
+var str3 = 'Hello, I\'m Khanh.'; // escape a character
+
+// Booolean (true, false)
+var isHidden = true;
+var isShow   = false;
+console.log(isHidden);
+console.log(isShow);
+
+// undefined
+var a;
+console.log(a);
+
+// null
+var b;
+b = null;
+console.log(a);
---
+++ Learn_javascript/Learn_width_video/Javascript_basic/4_Object/object.js
@@ -0,0 +1,35 @@
+// object
+// var a = {
+//     key : value
+// };
+
+
+var myCat = {
+    name: 'Tiger',
+    weight: 3,
+    isAlive: false
+};
+// camelCase 
+
+myCat.weight = 5;
+console.log(myCat.name);
+console.log(myCat);
+console.log(myCat['name']);
+
+
+
+
+var myBestGirld = {
+    nickName: 'Dog',
+    age: 28,
+    gender:'male'
+};
+
+myBestGirld['age'] = 20;
+console.log(myBestGirld['age']);
+
+
+
+// not attribute
+var a = {};
+
