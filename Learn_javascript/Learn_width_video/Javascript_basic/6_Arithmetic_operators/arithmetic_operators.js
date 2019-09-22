f30f49cc4edc33073d32bda60b8dc2b0d129e6e4
DUY KHANH <khanhlerduy@gmail.com>
2019-09-22 16:05:51 +0700

    Add file Learn_width_video/Javascript_basic/6_Arithmetic_operators/

---
+++ Learn_javascript/Learn_width_video/Javascript_basic/6_Arithmetic_operators/arithmetic_operators.js
@@ -0,0 +1,55 @@
+var x = 10;
+var y = 5;
+
+var c = x + y;
+console.log(c);
+
+var c = x - y;
+console.log(c);
+
+var c = x * y;
+console.log(c);
+
+var c = x / y;
+console.log(c);
+
+var c = x % y;
+console.log(c);
+
+
+
+console.log("----------------");
+
+// var a = 5;
+// a++;
+
+// var a = 5;
+// a--;
+
+// var a = 5;
+// --a;
+
+// var a = 5;
+// ++a;
+
+console.log("----------------");
+
+var a = 5;
+    a++ + ++a - --a + a--;
+
+    // 5 + ++a - --a + a--; //6
+    // 5 + 7 - --a + a--;//7
+    // 5 + 7 - 6 + a--;//6
+    // 5 + 7 - 6 + 6; // 5
+    // a = 0;
+
+
+
+var b = 10;
+    b++ * b-- * --b + ++b - b-- - --b;
+
+//  10(11) * 11(10) * 9 + 10 - 10(9) - 8; 
+// a = 982;
+
+
+
