6901292a416c62beda5d297fc3261d57c2b273c3
DUY KHANH <khanhlerduy@gmail.com>
2019-09-22 20:37:48 +0700

    Learn_width_video/Javascript_basic/8_Function/

---
+++ Learn_javascript/Learn_width_video/Javascript_basic/8_Function/function.js
@@ -0,0 +1,39 @@
+// Function
+function calculateTriangleSquare(a, h) {
+    return a * h / 2;
+}
+
+// Tính diện tích tam giác có chiều cao là
+// h = 5, cạnh đáy a = 10
+
+calculateTriangleSquare(10, 5);
+
+// Tính diện tích hình tròn
+// S = r * r * 3.14;
+
+// Tinh diện tích hình tròn bán kính r = 10;
+function calculateDiscSquare(r){
+    return r * r * 3.14;
+}
+
+calculateDiscSquare(10);
+calculateDiscSquare(20);
+calculateDiscSquare(30);
+
+// Tính diện tích hình vuông
+// S = c * c;
+
+function calculateAreaSquare(c){
+    return c * c;
+}
+
+calculateAreaSquare(5 );
+
+// Tính diện tích hình chữ nhật
+// S = d * r
+
+function calculateRectangleSquare(d,r){
+    return d * r;
+}
+
+calculateRectangleSquare(10,5);
