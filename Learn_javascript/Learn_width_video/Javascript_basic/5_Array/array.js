70ded76fb3ea50b48be095e99e9ddd40dd3d7d1e
DUY KHANH <khanhlerduy@gmail.com>
2019-09-22 15:35:11 +0700

    Add file Learn_width_video/Javascript_basic/5_Array/

---
+++ Learn_javascript/Learn_width_video/Javascript_basic/5_Array/array.js
@@ -0,0 +1,23 @@
+// Array
+
+var employee1 = {
+    name    : 'Hoàng',
+    gender  : 'Nữ',
+    age     : 22 
+}
+
+var employee2 = {
+    name    : 'Khánh',
+    name    : 'khanh',
+    gender  : 'Nam',
+    age     : 21
+}
+
+var employee3 = {
+    khanh
+}
+
+var arrayEmployee = [employee1, employee2, employee3];
+console.log(arrayEmployee);
+console.log(arrayEmployee[0]);
+console.log(arrayEmployee[0].name);
