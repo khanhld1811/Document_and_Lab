17353467dfbecb4c0f5f760feec0e8a58f2fde6a
= <=>
2019-09-23 22:55:52 +0700

    add new file 15_Array_map/

---
+++ Learn_javascript/15_Array_map/index.js
@@ -0,0 +1,18 @@
+// array.map
+
+var numbers = [1,2,3,4];
+var squareNumbers = numbers.map(function(x){
+    return x * x;
+});
+
+squareNumbers;
+
+//bai tap
+
+var rectangles = [
+    {width:10, height:5},
+    {width:10, height:20},
+    {width:5, height:15}
+];
+
+
