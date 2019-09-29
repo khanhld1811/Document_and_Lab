1048acc9e6f43eb5e4a6c4baa1dcc4274cf90a11
= <=>
2019-09-29 21:04:46 +0700

    add new file 33_switch_case

---
+++ Learn_javascript/33_switch_case/index.js
@@ -0,0 +1,50 @@
+//bronze, silver, gold, diamond
+// bronze: 2%
+// silver: 5%
+// gold: 8%
+// diamond: 10%
+var memberCard = {
+    tier: 'diamond'
+};
+
+function getTotal(price, card){
+    var discountRate;
+
+    switch(card.tier){
+        case 'bronze':
+            discountRate = 0.02;
+            break;
+        case 'silver':
+            discountRate = 0.05;
+            break;
+        case 'gold':
+            discountRate = 0.08;
+            break;
+        default:
+            discountRate = 0.1;
+            break;
+    } 
+
+    return price * (1-discountRate);
+}
+
+var total = getTotal(500000, memberCard);
+console.log(total);
+
+// red, green, orange
+var trafficLight = 'orange';
+
+function goOrNotInVietNam(lightValue){
+    switch(lightValue){
+        case 'green':
+        case ' orange':
+            console.log('go');
+        default: 
+            console.log('stop');
+            break;
+    }
+}
+
+goOrNotInVietNam(trafficLight);
+
+
