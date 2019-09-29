f6f4d6495c211d02a7bc9efe37077c353835b7d8
= <=>
2019-09-29 21:13:47 +0700

    add new file 35_while_do_while

---
+++ Learn_javascript/35_while_do_while/index.js
@@ -0,0 +1,17 @@
+// for (var i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+var i = 1;
+
+while(i <= 10){
+    console.log(i);
+    ++i;
+}
+
+console.log('yyy', i);
+
+do{
+    console.log(i);
+    ++i;
+} while(i <= 10);
