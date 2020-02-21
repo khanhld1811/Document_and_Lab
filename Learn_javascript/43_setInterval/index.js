be64cffbf0f89f038dd58a75303f07eb3cc2de44
khanhld <khanhld@vnext.com.vn>
2020-02-21 10:29:38 +0700

    Add file Learn_javascript/43_setInterval/

---
+++ Learn_javascript/43_setInterval/index.js
@@ -0,0 +1,30 @@
+// var i = 0;
+
+// var intervalId = setInterval(function () {
+//     i++;
+//     console.log(i);
+//     if (i === 5) {
+//         clearInterval(intervalId);
+//     }
+// }, 1000);
+
+
+//Write a function count from 1 to 10
+// return a promise
+function countFrom(a, b) {
+    return new Promise(function (resolve, reject) {
+        var id = setInterval(function () {
+            console.log(a);
+            ++a;
+            if (a >= b) {
+                console.log(a)
+                clearInterval(id)
+                resolve()
+            }
+        }, 1000);
+    });
+}
+
+countFrom(1, 10).then(function () {
+    console.log('Done');
+})
