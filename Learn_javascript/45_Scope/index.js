8c1e096693cfc783738f63da47a31c2933826f56
khanhld <khanhld@vnext.com.vn>
2020-02-22 12:04:36 +0700

    Add file Learn_javascript\45_Scope

---
+++ Learn_javascript/45_Scope/index.js
@@ -0,0 +1,18 @@
+// scope
+// 1. Global scope
+// 2. Local scope
+// when a function is executed -> create a new scope
+   var a = 1;
+   var b = 2;
+
+   function random(x) {
+	var b = Math.random();
+	   console.log(a);
+	   console.log(b + x);
+   }
+   function doSomething(){
+	   a = 5
+   }
+
+   doSomething();
+   var y = random(a);
