326d1ba9b2684d76728420dbd3a687b8bbfa7c0a
khanhld1811 <banhmismile@gmail.com>
2020-03-16 14:07:02 +0700

    Add ReactJS - 06_Component

---
+++ Learn_ReactJs/06_Component/todo-list-component/src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class TodoItem extends Component {
+    render() {
+        return(
+            <div className="TodoItem">
+                <h1>Đi NET</h1>
+                <p>Đi Hoc</p>
+            </div>
+        )
+    }
+}
+
+export default TodoItem;
